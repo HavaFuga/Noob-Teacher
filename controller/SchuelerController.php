ae0ef02fec24ba434a3b9adbec6aa67811d81920
alierendapar <32871483+alierendapar@users.noreply.github.com>
2017-10-23 09:48:21 +0200

    dadada

---
+++ controller/SchuelerController.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once '../repository/SchuelerRepository.php';
+
+/**
+ * Siehe Dokumentation im DefaultController.
+ */
+class SchuelerController
+{
+    public function index()
+    {
+        $schuelerRepository = new SchuelerRepository();
+
+        $view = new View('schueler_index');
+        $view->title = 'Schüler';
+        $view->heading = 'Schüler';
+        $view->schueler = $schuelerRepository->readAll();
+        $view->display();
+    }
+
+    public function create()
+    {
+        $view = new View('schueler_create');
+        $view->title = 'Schüler erstellen';
+        $view->heading = 'Schüler erstellen';
+        $view->display();
+    }
+
+    public function doCreate()
+    {
+        if ($_POST['send']) {
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $email = test_input($_POST["email"]);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format"; 
+            }
+
+            $schuelerRepository = new SchuelerRepository();
+            $schuelerRepository->create($firstName, $lastName, $email);
+        }
+
+        // Anfrage an die URI /user weiterleiten (HTTP 302)
+        header('Location: /user');
+    }
+
+    public function delete()
+    {
+        $schuelerRepository = new SchuelerRepository();
+        $schuelerRepository->deleteById($_GET['id']);
+
+        // Anfrage an die URI /user weiterleiten (HTTP 302)
+        header('Location: /user');
+    }
+}
