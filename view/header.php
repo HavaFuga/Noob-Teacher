e793a8f7a88b16302ff835205b08681677b0b2c9
HavaFuga <32871313+HavaFuga@users.noreply.github.com>
2017-10-24 16:49:37 +0200

    dsf

--- view/header.php
+++ view/header.php
@@ -30,11 +30,11 @@
         <div class="content-right col-md-4">
             <nav class="navigation" id="navi">
                 <ul class="">
-                    <a class="btn btn-sm animated-button thar-three" href="/"><li>Home</li></a>
-                    <a class="btn btn-sm animated-button thar-three" href="/schueler"><li>Schüler</li></a>
-                    <a href="/schueler/create"><li>Schüler erstellen</li></a>
-                    <a href="/schueler/edit"><li>Schüler bearbeiten</li></a>
-                    <a href="/schueler/help"><li>Hilfe</li></a>
+                    <a class="btn animated-button thar-three" href="/"><li>Home</li></a>
+                    <a class="btn animated-button thar-three" href="/schueler"><li>Schüler</li></a>
+                    <a class="btn animated-button thar-three" href="/schueler/create"><li>Schüler erstellen</li></a>
+                    <a class="btn animated-button thar-three" href="/schueler/edit"><li>Schüler bearbeiten</li></a>
+                    <a class="btn animated-button thar-three" href="/schueler/help"><li>Hilfe</li></a>
                 </ul>
             </nav>
             <div id="hv"></div>
