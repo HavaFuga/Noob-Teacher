1412a30de4c245abe362bebc775efb473f98df1f
HavaFuga <32871313+HavaFuga@users.noreply.github.com>
2017-10-30 16:23:51 +0100

    favicon

--- view/header.php
+++ view/header.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?> | Bbc MVC</title>
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/images/Favicon.ico">
+
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
