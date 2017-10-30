167ffbfcc216b82ec5f59c5595b2226a54824613
alierendapar <32871483+alierendapar@users.noreply.github.com>
2017-10-30 08:48:26 +0100

    Ich habe auf der Home-Seite ein Bild hinzugefügt (tescher-ico)

--- view/default_index.php
+++ view/default_index.php
@@ -4,4 +4,8 @@
     <a href="/schueler/create"><button>Schüler erstellen</button></a>
     <a href="/schueler/edit"><button>Schüler bearbeiten</button></a>
     <a href="/schueler/help"><button>Hilfe</button></a>
+    
+</div>
+<div class="teacher-icon">
+ <img id="teacher-icon" src="\images\teacher-icon.png">
 </div>
