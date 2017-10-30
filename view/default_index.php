6bc1d355478619cc33767fca17d794b26de1299b
HavaFuga <32871313+HavaFuga@users.noreply.github.com>
2017-10-30 08:56:18 +0100

    da

--- view/default_index.php
+++ view/default_index.php
@@ -1,10 +1,10 @@
 <p class="home-text">Easy Help for managing Students.</p>
+<!-- Home-Navigation-->
 <div class="nav-home">
     <a href="/schueler"><button>Schüler</button></a>
     <a href="/schueler/create"><button>Schüler erstellen</button></a>
     <a href="/schueler/edit"><button>Schüler bearbeiten</button></a>
     <a href="/schueler/help"><button>Hilfe</button></a>
-    
 </div>
 <div class="teacher-icon">
  <img id="teacher-icon" src="\images\teacher-icon.png">
--- view/header.php
+++ view/header.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
     <title><?= $title ?> | Bbc MVC</title>
 
     <!-- Bootstrap core CSS -->
@@ -14,31 +11,27 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/style.css" type="text/css" rel="stylesheet">
+
     <!-- JS -->
       <script href="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
       <script language="javascript" type="text/javascript" src="/js/javascript.js"></script>
+    <!-- Begin Header -->
   </head>
   <body>
       <div class="logo" >
-          <a href="/"><img id="logo" src="\images\Logo.png"></a>
+          <a href="/"><img id="logo" src="/images/Logo.png"></a>
       </div>
-
     <div class="container">
         <?php if(!isset($is_home) || $is_home == false){ ?>
         <div class="content-right col-md-4">
             <nav class="navigation" id="navi">
                 <ul class="">
-
-                    
-                     
-
                     <a class="btn animated-button thar-three" href="/"><li>Home</li></a>
                     <a class="btn animated-button thar-three" href="/schueler"><li>Schüler</li></a>
                     <a class="btn animated-button thar-three" href="/schueler/create"><li>Schüler erstellen</li></a>
                     <a class="btn animated-button thar-three" href="/schueler/edit"><li>Schüler bearbeiten</li></a>
                     <a class="btn animated-button thar-three" href="/schueler/help"><li>Hilfe</li></a>
-
                 </ul>
             </nav>
             <div id="hv"></div>
